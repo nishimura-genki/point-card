3cc4bb7183cc13481c74355c962be75e8ee590ba
Kento Takyu <33660146+tkyawa@users.noreply.github.com>
2020-12-24 18:38:27 +0900

    Revert "Revert "Singleuser""

--- account/admin.py
+++ account/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import User, Customer, Shop, PointCard
+from .models import User, Customer, Shop, PointCard, PointCardLog
 
 admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(Shop)
 admin.site.register(PointCard)
+admin.site.register(PointCardLog)
--- account/urls.py
+++ account/urls.py
@@ -51,4 +51,6 @@ urlpatterns = [
         template_name='account/does_not_have_stamp.html'), name='does_not_have_stamp'),
     path('customize_point_card/', views.CustomizePointCardView.as_view(),
          name='customize_point_card'),
+    path('point_card_log/',views.PointCardLogListView.as_view(),
+          name='point_card_log')
 ]
